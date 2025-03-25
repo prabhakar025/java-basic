2ac5c7dec0e2bc48dae290545c7e81d65fafab76
prabhakar025 <prabhakar263801@gmail.com>
2025-03-26 03:01:36 +0530

    largenumber

---
+++ checklarge.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class checklarge
+{
+public static void main(String[]args)
+{
+int a=1;
+int b=2;
+if(a>=b){
+System.out.println("a is largestof 2");
+}
+else{
+System.out.println("b is largestof 2");
+}
+}
+}
