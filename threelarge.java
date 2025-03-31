06029ae974fcc82559a3ea22ad4cf49e135d3e06
prabhakar025 <prabhakar263801@gmail.com>
2025-03-31 13:21:54 +0530

    checkthree large no

---
+++ threelarge.java
@@ -0,0 +1,17 @@
+public class threelarge
+{
+public static void main(String []args)
+{
+int a=10,b=80,c=70;
+if(a>b && a>c){
+System.out.println("a is greater");
+}
+else if(b>a && b>c)
+{
+System.out.print("b is greater");
+}
+else{
+System.out.println("c is greater");
+}
+}
+}
